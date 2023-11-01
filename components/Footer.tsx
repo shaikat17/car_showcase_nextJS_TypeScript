7ce18f218392e12d5bfe94d2eb28b36a6a21c1ef
shaikat17 <shaikatpal56@gmail.com>
2023-11-01 14:03:44 +0600

    Footer section done.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,6 @@
+import { footerLinks } from '@/constants'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
@@ -12,6 +14,23 @@ const Footer = () => {
           All rights reserved &copy;
         </p>
       </div>
+      <div className="footer__links">
+        {footerLinks.map(link => (
+          <div key={link.title} className="footer__link">
+            <h3 className='font-bold'>{link.title}</h3>
+            {link.links.map(item => (
+              <Link key={item.title} href={item.url} className='text-gray-500'>{item.title}</Link>
+            ))}
+          </div>
+        ))}
+      </div>
+      </div>
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-300 sm:px-16 px-6 py-10">
+        <p>{`${new Date().getFullYear()} CarHub. All Rights Reserved.`}</p>
+              <div className="footer__copyrights-link">
+                <Link href="/" className='text-gray-500'>Privacy Policy</Link>
+                <Link href="/" className='text-gray-500'>Terms of Use</Link>
+              </div>
       </div>
     </footer>
   )
