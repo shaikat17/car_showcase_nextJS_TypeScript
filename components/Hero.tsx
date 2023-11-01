c4916a4338af64928dd874c3cbfb16c53c5c83be
shaikat17 <shaikatpal56@gmail.com>
2023-11-01 12:43:52 +0600

    Hero section done.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,7 +22,8 @@ const Hero = () => {
         </div>
         <div className="hero__image-container">
             <div className="hero__image">
-                <Image src={} />
+                <Image src="/hero.png" alt="hero" fill priority={false} className="object-contain" />
+                <div className="hero__image-overlay"></div>
             </div>
         </div>
     </div>
