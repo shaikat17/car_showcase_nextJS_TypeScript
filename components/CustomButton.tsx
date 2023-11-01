a60fd4a12fd7b661a557c59d42962dc176fdd695
shaikat17 <shaikatpal56@gmail.com>
2023-11-01 11:34:45 +0600

    Hero section text part complete.

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,20 +1,21 @@
 "use client"
 
 
+import { CustomButtonProps } from "@/types"
 import Image from "next/image"
 
-const CustomButton = () => {
-  return (
-    <button 
-    disabled={false}
-    type={"button"}
-    className={"custom-btn"}
-    onClick={() => {}}>
-<span className={`flex-1`}>
-    Title
-</span>
-    </button>
-  )
+const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+    return (
+        <button
+            disabled={false}
+            type={"button"}
+            className={`custom-btn ${containerStyles}`}
+            onClick={handleClick}>
+            <span className={`flex-1`}>
+                {title}
+            </span>
+        </button>
+    )
 }
 
 export default CustomButton
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import Image from "next/image"
 import { CustomButton } from "."
 
 const Hero = () => {
+    const handleClick = () => {
+
+    }
   return (
     <div className="hero">
         <div className="flex-1 pt-36 padding-x">
@@ -11,7 +16,14 @@ const Hero = () => {
             <p className="hero__subtitle">
                 Streamline your car rental experience with our effortless booking process.
             </p>
-            <CustomButton />
+            <CustomButton title="Explore Cars"
+            containerStyles="bg-blue-500 text-white rounded-full mt-10"
+            handleClick={handleClick}/>
+        </div>
+        <div className="hero__image-container">
+            <div className="hero__image">
+                <Image src={} />
+            </div>
         </div>
     </div>
   )
